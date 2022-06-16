1f1cf11f77bb2b1fad25c71e609789d029ef1a5f
m-veloper <m_veloper@naver.com>
2022-06-17 02:19:21 +0900

    Dog Test Component 생성

---
+++ pages/dog-test-components.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Dog Test Components</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Dog Test Components",
+      meta: [
+        {
+          hid: "description",
+          name: "Next Ui Kit",
+          content: "Next Ui Kit is the best way",
+        },
+      ],
+    };
+  },
+
+  data() {
+    return {};
+  },
+  components: {
+  },
+};
+</script>

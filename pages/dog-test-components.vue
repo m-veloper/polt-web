fcf8b8a9f47fe3e50210ac0942861e283a3ee285
m-veloper <m_veloper@naver.com>
2022-06-21 23:45:38 +0900

    dog 컴포넌트 수정

--- pages/dog-test-components.vue
+++ pages/dog-test-components.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <p>Dog Test Components</p>
+  <div class="banner-wrapper">
+    <!-- 인트로 페이지 -->
+    <main v-if="page === 0">
+      <img src="/onepiece.jpeg" alt="원피스 캐릭터들">
+      <h1> 나와 닮은 원피스 캐릭터 찾기! </h1>
+      <h2> 나와 닮은 원피스 캐릭터는 누구일까요?</h2>
+      <Button text="테스트 시작!" :clickEvent="startTest"/>
+    </main>
+
+    <!-- 질문 페이지 -->
+    <main v-else-if="page < 5">
+      <Question />
+    </main>
+
+    <!-- 결과 페이지 -->
+    <main v-else-if = "page === 5">
+      <Result />
+    </main>
   </div>
 </template>
 
@@ -24,5 +40,16 @@ export default {
   },
   components: {
   },
+
+  computed: {
+    page() {
+      return this.$store.state.page;
+    },
+  },
+  methods:{
+    startTest(){
+      this.$store.commit("SET_PAGE",1)
+    }
+  }
 };
 </script>

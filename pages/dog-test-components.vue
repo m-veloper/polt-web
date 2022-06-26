382c0001da6991da7b05d9b64d9339a18b1226fd
m-veloper <m_veloper@naver.com>
2022-06-27 00:47:41 +0900

    도그 테스트, 결과 페이지 수정

--- pages/dog-test-components.vue
+++ pages/dog-test-components.vue
@@ -9,23 +9,18 @@
                   <v-col cols="12" md="7" lg="6" class="d-flex align-center">
                       <div class="text-center text-md-left">
 
-                          <main v-if="page === 0">
-                              <!--      <img src="/onepiece.jpeg" alt="원피스 캐릭터들">-->
-                              <h1 class="gugi-font" id="test-title"> Pet Owner License Test </h1>
-<!--                                    <h2> 나의 자격은?</h2>-->
+                        <!-- 인트로 페이지 -->
+                        <main v-if="page===0">
+                          <img src="/poster.jpeg" alt="해리포터" class="poster">
+                          <h1>나와 닮은 해리포터 캐릭터 찾기</h1>
+                          <TestButton text="Test start" :clickEvent="startTest"/>
+                        </main>
 
-                                    <TestButton text="테스트 시작!" :clickEvent="startTest" class=""/>
-                          </main>
+                        <!-- 질문 페이지 -->
+                        <main class="question" v-else-if="page <= 12">
+                          <Question />
+                        </main>
 
-                          <!-- 질문 페이지 -->
-                          <main v-else-if="page < 5">
-                              <Question />
-                          </main>
-
-                          <!-- 결과 페이지 -->
-                          <main v-else-if = "page === 5">
-                              <Result />
-                          </main>
                       </div>
                   </v-col>
               </v-row>
@@ -38,6 +33,15 @@
 </template>
 
 <script>
+import Question from "@/components/Question";
+// import Question from '~/components/Question.vue'
+
+/**
+ * 메인 페이지 안에 들어갈 것
+ * 1. 인트로 페이지
+ * 2. 조건부 렌더링을 통해 퀴즈 컴포넌트가 보여야 함
+ * 3. 결과는 페이지 이동 /result/_mbti
+ */
 export default {
   head() {
     return {
@@ -51,26 +55,19 @@ export default {
       ],
     };
   },
-
-  data() {
-    return {};
-  },
-  components: {
-  },
-
+  components: { Question },
   computed: {
     page() {
       return this.$store.state.page;
-    },
+    }
   },
-  methods:{
-    startTest(){
-      this.$store.commit("SET_PAGE",1)
+  methods: {
+    startTest() {
+      this.$store.commit("SET_PAGE", 1)
     }
   }
 };
 </script>
-
 
 <style>
     #test-title {
